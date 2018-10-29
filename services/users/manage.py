399896f000f05ac17c0553e4f037aca0c4c8c664
silent-sea1119 <sandragonzales56t@gmail.com>
2018-10-28 22:06:29 -0700

    Added coverage and linting

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,20 @@
 import unittest
 
 from flask.cli import FlaskGroup
+import coverage
 
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 
 app = create_app
 cli = FlaskGroup(create_app=create_app)
@@ -30,6 +41,21 @@ def seed_db():
     db.session.add(User('michael', 'hermanmu@gmail.com'))
     db.session.add(User('michaelherman', 'michael@mherman.org'))
     db.session.commit()
+
+
+@cli.command()
+def cov():
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == "__main__":
